05d712c175772ecb6f38a42a1d022f384b98bdf7
antjuntu <antjuntu@gmail.com>
2018-11-06 20:34:49 +0200

    4.3 - 4.7

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,9 +1,99 @@
 const blogs = require('./blogs')
 const listHelper = require('../utils/list_helper')
 
-test('dummy is called', () => {
-  const blogs = []
+describe('total likes', () => {
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+  test('when list has only one blog equals the likes of that', () => {
+    const listWithOneBlog = []
+    listWithOneBlog.push(blogs[0])
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(7)
+  })
+
+  test('when list is empty equals 0', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  test('when list has many blogs is calculated right', () => {
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(36)
+  })
+})
+
+describe('favorite blog', () => {
+  test('when list is empty returns undefined', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toBeUndefined()
+  })
+
+  test('when list has only one blog equals that blog', () => {
+    const listWithOneBlog = []
+    listWithOneBlog.push(blogs[0])
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual({
+      title: 'React patterns',
+      author: 'Michael Chan',
+      likes: 7
+    })
+  })
+
+  test('when list has many blogs is calculated right', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual({
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12
+    })
+  })
+})
+
+describe('most blogs', () => {
+  test('when list is empty return undefined', () => {
+    const result = listHelper.mostBlogs([])
+    expect(result).toBeUndefined()
+  })
+
+  test('when list has only one blog has that blogs author', () => {
+    const listWithOneBlog = []
+    listWithOneBlog.push(blogs[0])
+    const result = listHelper.mostBlogs(listWithOneBlog)
+    expect(result).toEqual({
+      author: 'Michael Chan',
+      blogs: 1
+    })
+  })
+
+  test('when list has many blogs is calculated right', () => {
+    const result = listHelper.mostBlogs(blogs)
+    expect(result).toEqual({
+      author: 'Robert C. Martin',
+      blogs: 3
+    })
+  })
+})
+
+describe('most likes', () => {
+  test('when list is empty returns undefined', () => {
+    const result = listHelper.mostLikes([])
+    expect(result).toBeUndefined()
+  })
+
+  test('when list has only one blog has that blogs author', () => {
+    const listWithOneBlog = []
+    listWithOneBlog.push(blogs[0])
+    const result = listHelper.mostLikes(listWithOneBlog)
+    expect(result).toEqual({
+      author: 'Michael Chan',
+      likes: 7
+    })
+  })
+
+  test('when list has many blogs is calculated right', () => {
+    const result = listHelper.mostLikes(blogs)
+    expect(result).toEqual({
+      author: 'Edsger W. Dijkstra',
+      likes: 17
+    })
+  })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,9 +1,80 @@
-const dummy = (blogs) => {
-  if (blogs) {
-    return 1
+const totalLikes = (blogs) => {
+  const likes = blogs.reduce((accumulator, current) => {
+    return accumulator + current.likes
+  }, 0)
+  return likes
+}
+
+const favoriteBlog = (blogs) => {
+  if (!blogs || blogs.length === 0) {
+    return undefined
+  }
+
+  let maxLikes = 0
+  let maxIndex = 0
+  blogs.forEach((blog, index) => {
+    if (blog.likes > maxLikes) {
+      maxLikes = blog.likes
+      maxIndex = index
+    }
+  })
+  return {
+    title: blogs[maxIndex].title,
+    author: blogs[maxIndex].author,
+    likes: blogs[maxIndex].likes
   }
 }
 
+const mostBlogs = (blogs) => {
+  if (!blogs || blogs.length === 0) {
+    return undefined
+  }
+
+  const authors = blogs.reduce((authors, currentBlog) => {
+    if (currentBlog.author in authors) {
+      authors[currentBlog.author]++
+    } else {
+      authors[currentBlog.author] = 1
+    }
+    return authors
+  }, {})
+  const authorList = []
+  for (const author in authors) {
+    authorList.push({
+      author: author,
+      blogs: authors[author]
+    })
+  }
+  return authorList.sort((a, b) => b.blogs - a.blogs)[0]
+}
+
+const mostLikes = (blogs) => {
+  if (!blogs || blogs.length === 0) {
+    return undefined
+  }
+
+  const authors = blogs.reduce((authors, currentBlog) => {
+    if (currentBlog.author in authors) {
+      authors[currentBlog.author] += currentBlog.likes
+    } else {
+      authors[currentBlog.author] = currentBlog.likes
+    }
+    return authors
+  }, {})
+
+  const authorList = []
+  for (const author in authors) {
+    authorList.push({
+      author: author,
+      likes: authors[author]
+    })
+  }
+  return authorList.sort((a, b) => b.likes - a.likes)[0]
+}
+
 module.exports = {
-  dummy
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
