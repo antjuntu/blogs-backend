8f66ba969f64e9055a0c0d41f502139fe84b111d
antjuntu <antjuntu@gmail.com>
2018-11-06 18:09:15 +0200

    4.3

---
+++ tests/list_helper.test.js
@@ -0,0 +1,9 @@
+const blogs = require('./blogs')
+const listHelper = require('../utils/list_helper')
+
+test('dummy is called', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+})
