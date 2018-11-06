4973a14649f3214e8494645a1ccd3366cdb36522
antjuntu <antjuntu@gmail.com>
2018-11-06 16:14:36 +0200

    4.1 and 4.2

---
+++ controllers/blogs.js
@@ -0,0 +1,28 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+module.exports = blogsRouter
