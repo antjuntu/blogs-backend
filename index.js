538a71f40cbdbf68ad3dc135d28b07fb788603e6
antjuntu <antjuntu@gmail.com>
2018-11-07 14:40:36 +0200

    Test environment configured

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -6,15 +7,13 @@ const mongoose = require('mongoose')
 
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 
 mongoose
-  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .connect(config.mongoUrl, { useNewUrlParser: true })
   .then(() => {
-    console.log('connected to database', process.env.MONGODB_URI)
+    console.log('connected to database', config.mongoUrl)
   })
   .catch(err => {
     console.log(err)
@@ -28,7 +27,17 @@ app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.error)
 
-const PORT = process.env.PORT || 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+const server = http.createServer(app)
+
+server.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`)
 })
+
+server.on('close', () => {
+  mongoose.connection.close()
+})
+
+module.exports = {
+  app,
+  server
+}
