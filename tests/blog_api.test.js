c960d2f6a7a3bd3d8ed0bd2dbbd40a0677fee9a3
antjuntu <antjuntu@gmail.com>
2018-11-13 15:07:36 +0200

    4.12

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,137 +2,127 @@ const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const { initialBlogs, nonExistingId, blogsInDb } = require('./test_helper')
 
-const initialBlogs = [
-  {
-    title: 'Testi 1',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5
-  },
-  {
-    title: 'Testi 2',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12
-  }
-]
 
-beforeAll(async () => {
-  await Blog.remove({})
+describe('when there is initially some blogs saved', async () => {
 
-  const blogObjects = initialBlogs.map(blog => new Blog(blog))
-  const promiseArray = blogObjects.map(blogObj => blogObj.save())
-  await Promise.all(promiseArray)
-})
+  beforeAll(async () => {
+    await Blog.remove()
 
-describe('GET /api/blogs', async () => {
-  test('blogs are returned as json', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
+    const blogObjects = initialBlogs.map(b => new Blog(b))
+    await Promise.all(blogObjects.map(b => b.save()))
   })
 
-  test('all blogs are returned', async () => {
-    const response = await api
-      .get('/api/blogs')
+  describe('GET /api/blogs', async () => {
+    test('all blogs are returned as json', async () => {
+      const blogsInDatabase = await blogsInDb()
 
-    expect(response.body.length).toBe(initialBlogs.length)
+      const response = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      expect(response.body.length).toBe(blogsInDatabase.length)
+
+      const returnedTitles = response.body.map(blog => blog.title)
+      blogsInDatabase.forEach(blog => {
+        expect(returnedTitles).toContain(blog.title)
+      })
+    })
   })
 
-  test('a specific blog is within the returned blogs', async () => {
-    const response = await api
-      .get('/api/blogs')
+  describe('POST /api/blogs', async () => {
+    test('a valid blog can be added', async () => {
+      const blogsAtStart = await blogsInDb()
 
-    const titles = response.body.map(blog => blog.title)
-    expect(titles).toContain('Testi 1')
+      const newBlog = {
+        title: 'New Blog',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+        likes: 19
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const blogsAfterOperation = await blogsInDb()
+
+      expect(blogsAfterOperation.length).toBe(blogsAtStart.length + 1)
+      const titles = blogsAfterOperation.map(b => b.title)
+      expect(titles).toContain('New Blog')
+    })
+
+    test('a blog without likes set is added with 0 likes', async () => {
+      const newBlogWithoutLikes = {
+        title: 'New Blog without likes',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html'
+      }
+
+      const blogsAtStart = await blogsInDb()
+
+      await api
+        .post('/api/blogs')
+        .send(newBlogWithoutLikes)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const blogsAfterOperation = await blogsInDb()
+
+      const addedBlog = blogsAfterOperation.find(blog => blog.title === 'New Blog without likes')
+
+      expect(blogsAfterOperation.length).toBe(blogsAtStart.length + 1)
+      expect(addedBlog).not.toBeUndefined()
+      expect(addedBlog.likes).toBe(0)
+      
+    })
+
+    test('a blog without title is not added', async () => {
+      const newBlog = {
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5
+      }
+
+      const blogsAtStart = await blogsInDb()
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+
+      const blogsAfterOperation = await blogsInDb()
+
+      expect(blogsAfterOperation.length).toBe(blogsAtStart.length)
+    })
+
+    test('a blog without url is not added', async () => {
+      const newBlog = {
+        title: 'Testi 1',
+        author: 'Edsger W. Dijkstra',
+        likes: 5
+      }
+
+      const blogsAtStart = await blogsInDb()
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+
+      const blogsAfterOperation = await blogsInDb()
+
+      expect(blogsAfterOperation.length).toBe(blogsAtStart.length)
+    })
   })
-})
 
-describe('POST /api/blogs', async () => {
-  test('a valid blog can be added', async () => {
-    const newBlog = {
-      title: 'New Blog',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-      likes: 19
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-
-    const response = await api.get('/api/blogs')
-
-    const titles = response.body.map(blog => blog.title)
-
-    expect(response.body.length).toBe(initialBlogs.length + 1)
-    expect(titles).toContain('New Blog')
-  })
-
-  test('a blog without likes set is added with 0 likes', async () => {
-    const newBlogWithoutLikes = {
-      title: 'New Blog without likes',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html'
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlogWithoutLikes)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-
-    const response = await api.get('/api/blogs')
-
-    const addedBlog = response.body.find(blog => blog.title === 'New Blog without likes')
-
-    expect(addedBlog).not.toBeUndefined()
-    expect(addedBlog.likes).toBe(0)
-  })
-
-  test('a blog without title is not added', async () => {
-    const newBlog = {
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5
-    }
-
-    const blogsBefore = await api.get('/api/blogs')
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
-
-    const blogsAfter = await api.get('/api/blogs')
-
-    expect(blogsAfter.body.length).toBe(blogsBefore.body.length)
-  })
-
-  test('a blog without url is not added', async () => {
-    const newBlog = {
-      title: 'Testi 1',
-      author: 'Edsger W. Dijkstra',
-      likes: 5
-    }
-
-    const blogsBefore = await api.get('/api/blogs')
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
-
-    const blogsAfter = await api.get('/api/blogs')
-
-    expect(blogsAfter.body.length).toBe(blogsBefore.body.length)
+  afterAll(() => {
+    server.close()
   })
 })
 
-afterAll(() => {
-  server.close()
-})
+
---
+++ tests/test_helper.js
@@ -0,0 +1,35 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'Testi 1',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5
+  },
+  {
+    title: 'Testi 2',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12
+  }
+]
+
+const nonExistingId = async () => {
+  const blog = new Blog()
+  await blog.save()
+  await blog.remove()
+
+  return blog._id.toString()
+}
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs
+}
+
+module.exports = {
+  initialBlogs,
+  nonExistingId,
+  blogsInDb
+}
