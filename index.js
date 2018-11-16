49cd4fc01dd355026df138a8fe6b366e69c8b0a7
antjuntu <antjuntu@gmail.com>
2018-11-16 15:14:06 +0200

    4.20

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ mongoose
 app.use(cors())
 app.use(bodyParser.json())
 app.use(middleware.logger)
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
